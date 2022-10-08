4dd151df045e268f9572ddbfc8739eabe1a21059
L-Jones-hub <105738902+L-Jones-hub@users.noreply.github.com>
2022-10-08 10:43:49 -0500

    Added save button functionality

--- assets/script.js
+++ assets/script.js
@@ -19,3 +19,19 @@ function updateHour() {
 }
 
 updateHour();
+
+$(`.saveButton`).on(`click`, function () {
+  var value = $(this).siblings(`.description`).val();
+  var time = $(this).parent().attr(`id`);
+  localStorage.setItem(time, value);
+});
+
+$(`.time-block`).each(function () {
+  var number = $(this).attr(`id`);
+  var tasks = localStorage.getItem(number);
+  var taskDescription = $(this).children(`.description`);
+
+  if (tasks != null) {
+    taskDescription.val(tasks);
+  }
+});
