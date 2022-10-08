50848cec289dd3a83ca7714e31c272514a3d3795
L-Jones-hub <105738902+L-Jones-hub@users.noreply.github.com>
2022-10-08 10:37:28 -0500

    Added function to update time-block colors

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,21 @@
 var todayEl = $(`#currentDay`);
 var moment = moment();
+var currentHour = moment.hours();
+console.log(currentHour);
 
 todayEl.text(moment.format(`dddd, MMMM DD, YYYY`));
+
+function updateHour() {
+  $(`.time-block`).each(function () {
+    var rowHour = parseInt($(this).attr("id"));
+    if (rowHour > currentHour) {
+      $(this).addClass(`future`);
+    } else if (rowHour == currentHour) {
+      $(this).addClass(`present`);
+    } else {
+      $(this).addClass(`past`);
+    }
+  });
+}
+
+updateHour();
